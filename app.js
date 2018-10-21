085b2397bc03feb7ed028bc976dabc250c218926
Joseph Woolever <joseph.w.woolever@gmail.com>
2018-10-20 19:52:01 -0500

    loan calculations completed

--- app.js
+++ app.js
@@ -17,9 +17,20 @@ function calcResults(e) {
     const principal = parseFloat(amount.value);
     //interest value as a float then calculated
     const calcInterest = parseFloat(interest.value) / 100 / 12;
-    //years value as a float
-    const calcPayments = parseFloat(years.value);
+    //years value as a float then calculated
+    const calcPayments = parseFloat(years.value) * 12;
 
+    //monthly calculated
+    const x = Math.pow(1 + calcInterest, calcPayments);
+    const monthly = (principal * x * calcInterest) / (x-1);
+
+    if(isFinite(monthly)) {
+        monthlyPay.value = monthly.toFixed(2);
+        totalPay.value = (monthly * calcPayments).toFixed(2);
+        totalInterest.value = ((monthly * calcPayments) - principal).toFixed(2);
+    } else {
+        console.log('check for errors');
+    }
 
     e.preventDefault();
 }
